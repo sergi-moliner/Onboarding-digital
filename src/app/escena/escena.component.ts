f2ea533f61c9db7119990c0b4cc80fc1cd8909ff
sergi.moliner <sergilopezmoliner@gmail.com>
2024-04-21 14:34:11 +0200

    Slide navigation functionality in EscenaComponent

--- src/app/escena/escena.component.ts
+++ src/app/escena/escena.component.ts
@@ -11,4 +11,22 @@ import { iStep } from '../istep.interface';
 })
 export class EscenaComponent {
   @Input() frases: iStep[] = [];
+  currentStep : number = 0;
+
+  backgrounds: { image: string, color: string }[] = [
+    { image: '../../assets/images/time_managment.svg', color: '#0d9ea6' },
+    { image: '../../assets/images/programming.svg', color: '#d5dbdb' },
+    { image: '../../assets/images/meditation.svg', color: '#edc25c' },
+  ];
+
+  next(): void {
+    if (this.currentStep < this.frases.length - 1) {
+      this.currentStep++;
+    }
+  }
+  prev(): void {
+    if (this.currentStep > 0) {
+      this.currentStep--;
+    }
+  }
 }
